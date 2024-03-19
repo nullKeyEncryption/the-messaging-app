700ec56da857b7ee522f1d522c529a806e4b2986
Omer <omerfarukyildirim48@gmail.com>
2024-03-19 11:32:29 -0400

    Updated to match new design

--- html/eventListeners.js
+++ html/eventListeners.js
@@ -1,13 +1,16 @@
 //Add event listeners
+//This function is called after the browser has loaded the web page
 document.addEventListener('DOMContentLoaded', function() {
-    //This function is called after the browser has loaded the web page
   
-    //add listener to buttons
+    document.getElementById('loginButton').addEventListener('click', userLogin)
+    document.getElementById('signupButton').addEventListener('click', userSignup)
+    document.getElementById('logoutButton').addEventListener('click', userLogout)
+
+    document.getElementById('messageBox').addEventListener('input', resizeMessageBox)
     document.getElementById('send_button').addEventListener('click', sendMessage)
-    document.getElementById('username_button').addEventListener('click', validateUsername)
     document.getElementById('clear_button').addEventListener('click', clearMessages)
   
-    //add keyboard handler for the document as a whole, not separate elements.
+    //add handlers for the document as a whole, not separate elements.
     document.addEventListener('keydown', handleKeyDown)
   })
   
