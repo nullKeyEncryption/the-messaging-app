c5d6e03d4727d157c52f9bb4bdf6f9d9a3fcdd1d
Omer Yildirim <omerfarukyildirim48@gmail.com>
2024-03-26 12:47:08 -0400

    Add new event listeners for new buttons

--- html/eventListeners.js
+++ html/eventListeners.js
@@ -11,23 +11,27 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('signupButton').addEventListener('click', userSignup)
     document.getElementById('logoutButton').addEventListener('click', userLogout)
 
-  // Main page messaging buttons
-  document.getElementById('messageBox').addEventListener('input', resizeMessageBox)
-  document.getElementById('send_button').addEventListener('click', sendMessage)
-  document.getElementById('clear_button').addEventListener('click', clearMessages)
-  
-  //add handlers for the page as a whole, not to separate elements.
-  document.addEventListener('keydown', handleKeyDown)
+    // Main page messaging buttons
+    document.getElementById('messageBox').addEventListener('input', resizeMessageBox)
+    document.getElementById('send_button').addEventListener('click', sendMessage)
+    document.getElementById('clear_button').addEventListener('click', clearMessages)
+    
+    //add handlers for the page as a whole, not to separate elements.
+    document.addEventListener('keydown', handleKeyDown)
   }
 
   // Sign up page buttons
   else if(document.getElementById('signup-submit-button')){
     document.getElementById('signup-submit-button').addEventListener('click', signupRequest)
+    document.getElementById('toggle-password').addEventListener('click', toggleShowPassword)
+    document.addEventListener('keydown', submitSignup) // Enter button to submit form
   }
 
   // Log in page buttons
   else if(document.getElementById('login-submit-button')){
     document.getElementById('login-submit-button').addEventListener('click', loginRequest)
+    document.getElementById('toggle-password').addEventListener('click', toggleShowPassword)
+    document.addEventListener('keydown', submitLogin) // Enter button to submit form
   }
 })
   
